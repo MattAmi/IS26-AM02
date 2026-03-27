6c1a1820eff61ce3f4ada66ab857fb8303fa91ba
Husnain Arshed <10973025@polimi.it>
2026-03-27 23:38:14 +0100

    feat: finished ShamanicImmunityEffect()

---
+++ src/main/java/it/polimi/ingsw/am02/model/BuildingEffects/ShamanicImmunityEffect.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.am02.model.BuildingEffects;
+
+import it.polimi.ingsw.am02.model.BuildingEffect;
+import it.polimi.ingsw.am02.model.EffectVisitor;
+import it.polimi.ingsw.am02.model.Enumerations.EventType;
+import it.polimi.ingsw.am02.model.EventObserver;
+import it.polimi.ingsw.am02.model.Tribu;
+
+public class ShamanicImmunityEffect implements BuildingEffect, EventObserver {
+    final Tribu tribu;
+
+    public ShamanicImmunityEffect(Tribu tribu) {
+        this.tribu = tribu;
+    }
+
+    @Override
+    public void accept(EffectVisitor v) {
+        v.visitEventObserver(this);
+    }
+
+    @Override
+    public void EventStart(EventType eventType) {
+        if(eventType == EventType.SHAMANIC_RITUAL){
+            tribu.setImmuneToShamanicPenality(true);
+        }
+    }
+
+    public void EventEnd(EventType eventType){
+        if(eventType == EventType.SHAMANIC_RITUAL){
+            tribu.setImmuneToShamanicPenality(false);
+        }
+    }
+}

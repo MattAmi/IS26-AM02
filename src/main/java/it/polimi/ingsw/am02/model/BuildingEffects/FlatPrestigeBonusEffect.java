6326a37adb672731e817c77d4314e802f75918e6
Husnain Arshed <10973025@polimi.it>
2026-03-27 16:39:25 +0100

    feat: finished FlatPrestigeBonusEffect()

--- src/main/java/it/polimi/ingsw/am02/model/BuildingEffects/FlatPrestigeBonusEffect.java
+++ src/main/java/it/polimi/ingsw/am02/model/BuildingEffects/FlatPrestigeBonusEffect.java
@@ -1,4 +1,30 @@
 package it.polimi.ingsw.am02.model.BuildingEffects;
 
-public class FlatPrestigeBonusEffect {
+import it.polimi.ingsw.am02.model.BuildingEffect;
+import it.polimi.ingsw.am02.model.EffectVisitor;
+import it.polimi.ingsw.am02.model.Enumerations.PhaseType;
+import it.polimi.ingsw.am02.model.PhaseObserver;
+import it.polimi.ingsw.am02.model.Tribu;
+
+public class FlatPrestigeBonusEffect implements BuildingEffect, PhaseObserver {
+
+    final Tribu tribu;
+    final int bonusPP;
+
+    public FlatPrestigeBonusEffect(Tribu tribu, int bonusPP) {
+        this.tribu = tribu;
+        this.bonusPP = bonusPP;
+    }
+
+    @Override
+    public void accept(EffectVisitor visitor) {
+        visitor.visitPhaseObserver(this);
+    }
+
+    @Override
+    public void onPhaseChange(PhaseType newPhase) {
+        if(newPhase == PhaseType.END_GAME){
+            tribu.addPrestigePoints(bonusPP);
+        }
+    }
 }

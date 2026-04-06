15e29f35c41272d42386446d77ff534a56f8c341
MattAmi <matteo.amico@mail.polimi.it>
2026-04-06 10:10:51 +0200

    - Game attributes (not all of them) - Game constructor - interface GameState - abstract class BaseState - private class SetUpState - initializeBoard(), randomizeInitialTurnOrder(), transitionTo()

--- src/main/java/it/polimi/ingsw/am02/model/Game.java
+++ src/main/java/it/polimi/ingsw/am02/model/Game.java
@@ -1,12 +1,140 @@
 package it.polimi.ingsw.am02.model;
 
+import it.polimi.ingsw.am02.model.Enumerations.PhaseType;
+import it.polimi.ingsw.am02.model.Enumerations.Totem;
+
+import java.util.*;
+
+import static java.util.Collections.shuffle;
+
 public class Game {
+
+    private final String gameID;
+    private final int numPlayers;
+    private final Map<String, Player> players;
+    private GameBoard gameBoard;
+    private GameState currentState;
+    private String currentPlayerNickname;
+    private List<String> turnOrder;
+
+    private List<PhaseObserver> phaseObservers;
+    private boolean isExtraTurnMode;
+
+    private List<GameObserver> observers;
+
+
+
+
+    public Game(String gameID, List<String> nicknames; Map<String, Totem> chosenTotems) {
+
+        this.gameID = gameID;
+        this.numPlayers = nicknames.size();
+
+        this.players = new LinkedHashMap<>();
+        for(String nickname : nicknames) {
+            Totem totem = chosenTotems.get(nickname);
+            this.players.put(nickname, new Player(nickname, totem));
+        }
+
+        transitionTo(new SetUpState());
+    }
+
+    private void initializeBoard() { // Initializes GameBoard
+        gameBoard = new GameBoard(numPlayers);
+    }
+
+    private void randomizeInitialTurnOrder() {
+        // (a) Shuffles the nickname list
+        turnOrder = new ArrayList<>(players.keySet());
+        shuffle(turnOrder);
+
+        // (b) Sets the first player that has right to play
+        currentPlayerNickname = turnOrder.get(0);
+
+        // (c) Builds a List<Player> using the suffled turnOrder list
+        List<Player> orderedPlayers = new ArrayList<>();
+        for(String nickname : turnOrder) {
+            orderedPlayers.add(players.get(nickname));
+        }
+
+        // (d) Delegates the turn order setup to GameBoard
+        gameBoard.setUpInitialTurnOrder(orderedPlayers);
+    }
+
+
+    private void transitionTo(GameState nextState) {
+        this.currentState = nextState;
+        currentState.onEntry();
+    }
+
 
     public static void attachPhaseObserver(PhaseObserver effect) {
         // TODO: Husnain
     }
 
+    public static void notifyPhaseObservers(PhaseType phase) {
+        // TODO
+    }
+
     public static GameBoard getGameBoard() {
     }
+
+
+    // State Pattern with "Inner Classes"
+    private interface GameState {
+        public void moveTotem(String nickname, char tileID);
+        public void resolveActions(String nickname, List<String> selectedIDs);
+        public void onEntry();
+    }
+
+    private abstract class BaseState implements GameState {
+
+        private final PhaseType phaseType;
+
+        BaseState(PhaseType phase) {
+            this.phaseType = phase;
+        }
+
+        public void moveTotem(String nickname, char tileID) {
+            throw new InvalidActionException(); // lancio eccezione siccome non è possibile fare questa mossa in questo momento
+        }
+
+        public void resolveActions(String nickname, List<String> selectedIDs) {
+            throw new InvalidActionException(); // lancio eccezione siccome non è possibile fare questa mossa in questo momento
+        }
+
+        public final void onEntry() {
+            notifyPhaseObservers(phaseType);
+            onEntryActions();
+        }
+
+        public abstract void onEntryActions();
+    }
+
+
+    // Concrete States
+    private class SetUpState extends BaseState {
+
+        public SetUpState() {
+            super(PhaseType.SETUP);
+        }
+
+        public void onEntryActions() {
+            initializeBoard();
+            randomizeInitialTurnOrder();
+
+            transitionTo(new TotemPlacementState());
+        }
+    }
+
+    private class TotemPlacementState extends BaseState {
+
+        public TotemPlacementState() {
+            super(PhaseType.TOTEM_PLACEMENT);
+        }
+
+
+    }
+
 }
 
--- src/main/java/it/polimi/ingsw/am02/model/GameBoard.java
+++ src/main/java/it/polimi/ingsw/am02/model/GameBoard.java
@@ -1,8 +1,16 @@
 package it.polimi.ingsw.am02.model;
 
+import java.util.List;
+
 public class GameBoard {
+
+    public GameBoard(int numPlayers) {
+    }
 
     public TurnOrderTile getTurnOrderTile() {
         return null;
+    }
+
+    public void setUpInitialTurnOrder(List<Player> orderedPlayers) {
     }
 }

c11cda7746fad8fb9509f2a0334b45d96d1d495d
Raed Abbas <raed.abbas@mail.polimi.it>
2026-04-07 13:39:04 +0200

    added/modified CharacterCard, with the interface and the 6 cardEffects (InventorEffect...)

---
+++ src/main/java/it/polimi/ingsw/am02/model/NoEffect.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.am02.model;
+
+public class NoEffect implements CharacterEffect {
+
+    //intentionally left empty, it serves in the cases where the character doesn't have a direct impact once drawn
+    //for example when i draw a hunter without a gigot, it doesn't have any immediate effect
+    @Override
+    public void applyEffect(Tribu tribu) {
+    }
+}

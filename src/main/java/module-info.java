71e1d487e24d7b0c251e893f26f839d0527eede2
MattAmi <matteo.amico@mail.polimi.it>
2026-04-16 08:55:26 +0200

    - modifiche post refactoring cartelle. Ho ri-runnato tutti i test e passano. Spero non si sia rotto niente

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,10 @@ module it.polimi.ingsw.am02 {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
 
-
     opens it.polimi.ingsw.am02 to javafx.fxml;
     exports it.polimi.ingsw.am02;
+
+    exports it.polimi.ingsw.am02.model;
+    exports it.polimi.ingsw.am02.model.enumerations;
+    exports it.polimi.ingsw.am02.model.buildingeffects;
 }

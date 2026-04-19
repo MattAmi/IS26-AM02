853055d2a38ff73ca53b8d6f372f8d2cc289d4b4
Husnain-Arshed <husnain.arshed@mail.polimi.it>
2026-04-20 01:46:14 +0200

    chore: made the project compilable leaving some TODOS patched with temporary fixes

--- src/main/java/it/polimi/ingsw/am02/server/network/rmi/RmiClientHandler.java
+++ src/main/java/it/polimi/ingsw/am02/server/network/rmi/RmiClientHandler.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.am02.server.network.rmi;
 
+import it.polimi.ingsw.am02.common.messages.events.Event;
 import it.polimi.ingsw.am02.server.network.ClientHandler;
 
 public class RmiClientHandler implements ClientHandler {
 
+    @Override
+    public void notify(Event event) {
+
+    }
 }

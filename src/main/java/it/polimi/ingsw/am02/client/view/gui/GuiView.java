57980b23f1de532ee5ee7500bf0e34f018c5e05e
MattAmi <matteo.amico@mail.polimi.it>
2026-04-28 12:23:40 +0200

    - GuiView: constructor and onGameModelCreated() (exact same structure of TuiView)

--- src/main/java/it/polimi/ingsw/am02/client/view/gui/GuiView.java
+++ src/main/java/it/polimi/ingsw/am02/client/view/gui/GuiView.java
@@ -1,6 +1,21 @@
 package it.polimi.ingsw.am02.client.view.gui;
 
+import it.polimi.ingsw.am02.client.model.GameModel;
+import it.polimi.ingsw.am02.client.model.LobbyModel;
 import it.polimi.ingsw.am02.client.view.AbstractClientView;
 
 public class GuiView extends AbstractClientView {
+
+    private final LobbyModel lobbyModel;
+    private GameModel gameModel; // null until GameStartedEvent
+
+    public GuiView(LobbyModel lobbyModel) {
+        this.lobbyModel = lobbyModel;
+        lobbyModel.addObserver(this);
+    }
+
+    public void onGameModelCreated(GameModel gameModel) {
+        this.gameModel = gameModel;
+        gameModel.addObserver(this);
+    }
 }
